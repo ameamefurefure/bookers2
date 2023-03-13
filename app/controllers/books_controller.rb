55b12564e000da790120855c22626257d8c87199
ameamefurefure <124484020+ameamefurefure@users.noreply.github.com>
2023-03-13 17:24:36 +0000

    bookのindexページの左側の作成中

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,17 @@
 class BooksController < ApplicationController
+  def new
+    @book = Book.new
+  end
+  
+  def create
+    list = Book.new(book_params) # データ受取＆新規登録
+    list.save # データベースに保存
+    redirect_to '/books/#{book.id}'
+  end
+  
+  
   def index
   end
-
 
   def show
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,10 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @books = @user.books
     # @books = @user.books.page(params[:page]) #ページネーションの為、こっちに変更?
-
+  end
+  
+  def user_params
+    params.require(:user).permit(:title, :body, :profile_image)
   end
 
   def edit
